04a94a9d67e3f7eb7100027167172a74a91b6ce9
LimaoC <80520563+LimaoC@users.noreply.github.com>
2021-07-30 22:48:05 +1000

    Update main.py
    Add functionality to generate random sudoku puzzles

--- main.py
+++ main.py
@@ -5,13 +5,41 @@ class Generator:
     """
     Generator is a class used for generating new games of sudoku.
     """
-    def __init__(self) -> None:
+    def __init__(self, min_clues: int) -> None:
         """
         Generates a random unique game of sudoku.
+
+        Parameters:
+            min_clues: the number of clues to have on the board (>= 17)
         """
-        # Create empty grid and generate a full solution
+        # Generate a full valid grid
         self._grid = [[0 for _ in range(9)] for _ in range(9)]
         self.generate()
+
+        clues = 81
+        rounds = 3
+
+        while rounds > 0 and clues >= min_clues:  # unique soln iff clues >= 17
+            # Get the row, column, and value of the next filled tile
+            row, col = self.get_random_filled()
+            tile_value = self._grid[row][col]
+
+            # Empty the filled tile and count the number of solutions that can
+            # be reached in the resulting puzzle state
+
+            self._grid[row][col] = 0
+            self._count = 0
+            self.count_solutions()
+
+            # Rollback if the resulting grid doesn't have a unique soln anymore
+            if self._count > 1:
+                self._grid[row][col] = tile_value
+                rounds -= 1
+            else:
+                clues -= 1
+
+        for i in self._grid:
+            print(i)
 
     def generate(self) -> bool:
         """
@@ -20,7 +48,7 @@ class Generator:
         Returns:
             (bool): True if the puzzle has been generated
         """
-        nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        nums = [i for i in range(1, 10)]
 
         row, col = self.get_next_blank()
 
@@ -58,6 +86,21 @@ class Generator:
 
         return (-1, -1)
 
+    def get_random_filled(self) -> tuple[int]:
+        """
+        Gets a randomly chosen filled tile in the puzzle.
+
+        Returns:
+            (tuple): tuple containing the (row, col) index of the random filled
+            tile
+        """
+        while True:
+            randrow = random.randint(0, 8)
+            randcol = random.randint(0, 8)
+
+            if self._grid[randrow][randcol] != 0:
+                return (randrow, randcol)
+
     def is_valid(self, guess: int, row: int, col: int) -> bool:
         """
         Determines whether inserting guess at (row, col) will yield a valid
@@ -90,3 +133,34 @@ class Generator:
                     return False
 
         return True
+
+    def count_solutions(self) -> None:
+        """
+        Counts the number of solutions that can be reached from the current
+        puzzle state.
+        """
+        # Keep a copy of the current grid state to rollback to after the count
+        grid_copy = self._grid.copy()
+        self._count = 0
+
+        def solve() -> bool:
+            row, col = self.get_next_blank()
+
+            if (row, col) == (-1, -1):
+                self._count += 1
+                return True
+            else:
+                for guess in range(1, 10):
+                    if self.is_valid(guess, row, col):
+                        self._grid[row][col] = guess
+
+                        if solve():
+                            # Check all possible solutions before finishing
+                            if guess == 9:
+                                return True
+
+                        self._grid[row][col] = 0
+
+        # Count the number of solutions and then rollback the grid state
+        solve()
+        self._grid = grid_copy
