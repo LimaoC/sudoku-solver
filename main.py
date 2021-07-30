c96b53ba0a9e4ea4a0b7d92d3996c210a45479d8
LimaoC <80520563+LimaoC@users.noreply.github.com>
2021-07-30 20:42:25 +1000

    Update main.py
    Updating code with OOP approach.
    - Add Generator class which is used to generate new sudoku puzzles

--- main.py
+++ main.py
@@ -1,154 +1,92 @@
-import timeit
+import random
 
 
-def is_valid(puzzle: list, guess: int, row: int, col: int) -> bool:
+class Generator:
     """
-    Determines whether inserting guess at (row, col) will yield a valid puzzle
-    state.
-
-    Parameters:
-        puzzle: puzzle to check valid state
-        guess: number to insert into puzzle
-        row: row to insert into
-        col: column to insert into
-
-    Returns:
-        (bool): True if puzzle state will be valid, False otherwise
+    Generator is a class used for generating new games of sudoku.
     """
-    # Checks corresponding row and column for duplicate values
-    if guess in puzzle[row]:
-        return False
-    if any(guess == puzzle[rows][col] for rows in range(9)):
-        return False
+    def __init__(self) -> None:
+        """
+        Generates a random unique game of sudoku.
+        """
+        # Create empty grid and generate a full solution
+        self._grid = [[0 for _ in range(9)] for _ in range(9)]
+        self.generate()
 
-    # Gets the index of the corresponding 3x3 square. For example, (5, 2) ->
-    # (1, 0), representing the 3x3 square in the 2nd row and 1st column.
+    def generate(self) -> bool:
+        """
+        Generates a random unique game of sudoku.
 
-    square_row, square_col = row // 3 * 3, col // 3 * 3
-    square_row_end, square_col_end = square_row + 3, square_col + 3
+        Returns:
+            (bool): True if the puzzle has been generated
+        """
+        nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    # Checks corresponding 3x3 square for duplicate values
-    for srow in range(square_row, square_row_end):
-        for scol in range(square_col, square_col_end):
-            if guess == puzzle[srow][scol]:
-                return False
+        row, col = self.get_next_blank()
 
-    return True
+        if (row, col) == (-1, -1):  # grid has been filled
+            return True
+        else:
+            # Attempt to fill next blank tile with a random number
+            random.shuffle(nums)
 
+            for num in nums:
+                if self.is_valid(num, row, col):
+                    self._grid[row][col] = num
 
-def get_next_blank(puzzle: list) -> tuple:
-    """
-    Gets the next blank tile in the puzzle (from top left to bottom right).
+                    if self.generate():  # recursively tries to fill next tile
+                        return True
 
-    Parameters:
-        puzzle: puzzle to grab blank tile from
+                    # If puzzle state is invalid, reset last guess
+                    self._grid[row][col] = 0
 
-    Returns:
-        (tuple): tuple containing the (row, col) of the blank tile, or (-1, -1)
-            if there are no more blank tiles.
-    """
-    for row in range(9):
-        for col in range(9):
-            if puzzle[row][col] == 0:
-                return (row, col)
+            return False
 
-    return (-1, -1)
+    def get_next_blank(self) -> tuple[int]:
+        """
+        Gets the next blank tile in the puzzle, working from top left to bottom
+        right.
 
+        Returns:
+            (tuple): tuple containing the (row, col) index of the next blank
+            tile, or (-1, -1) if there are no more blank tiles
+        """
+        for row in range(9):
+            for col in range(9):
+                if self._grid[row][col] == 0:
+                    return (row, col)
 
-def solve(puzzle: list) -> bool:
-    """
-    Solves the input sudoku using a backtracking algorithm.
+        return (-1, -1)
 
-    Parameters:
-        puzzle: puzzle to solve
+    def is_valid(self, guess: int, row: int, col: int) -> bool:
+        """
+        Determines whether inserting guess at (row, col) will yield a valid
+        puzzle state.
 
-    Returns:
-        (bool): True if the puzzle has been solved
-    """
-    row, col = get_next_blank(puzzle)
+        Parameters:
+            guess: number to check against puzzle state
+            row: row to insert guess into
+            col: column to insert guess into
 
-    if (row, col) == (-1, -1):  # Puzzle is filled
+        Returns:
+            (bool): True if puzzle state is valid after insertion, False
+            otherwise
+        """
+        grid = self._grid
+        # Check row and column for duplicate values
+        if (guess in grid[row] or
+                any(guess == grid[rows][col] for rows in range(9))):
+            return False
+
+        # Get index of the corresponding 3x3 square. For example, (5, 2) ->
+        # (1, 0), which is the 3x3 square in the 2nd row and 1st column.
+
+        square_row = (row // 3 * 3, (row // 3 * 3) + 3)
+        square_col = (col // 3 * 3, (col // 3 * 3) + 3)
+
+        for srow in range(square_row[0], square_row[1]):
+            for scol in range(square_col[0], square_col[1]):
+                if guess == grid[srow][scol]:
+                    return False
+
         return True
-    else:
-        for guess in range(1, 10):
-            if is_valid(puzzle, guess, row, col):
-                puzzle[row][col] = guess
-
-                if solve(puzzle):  # Recursively attempts to solve sudoku
-                    return True
-
-                # If puzzle state is invalid, reset last guess
-                puzzle[row][col] = 0
-
-        return False
-
-
-def print_grid(puzzle: list) -> None:
-    """
-    Prints the puzzle in a user-friendly format.
-
-    Parameters:
-        puzzle: puzzle to print
-    """
-    VERTICAL_BORDER = "|"
-    HORIZONTAL_BORDER = "-"
-    index = 0
-    row = 0
-
-    for num in range(11):
-        if num in (3, 7):  # Add horizontal border
-            print((HORIZONTAL_BORDER + " ") * 10 + HORIZONTAL_BORDER)
-        else:
-            for char in range(11):
-                if char in (3, 7):  # Add vertical border
-                    print(VERTICAL_BORDER + " ", end="")
-                elif char == 10:  # Last number in row
-                    print(str(puzzle[row][index]))
-                else:
-                    print(str(puzzle[row][index]) + " ", end="")
-                    index += 1
-            index = 0
-            row += 1
-
-
-def main():
-    """Main logic of program"""
-    # Valid input has all numbers from top left to bottom right in one string,
-    # and either a 0 or a whitespace for the blank tiles.
-
-    valid_input = False
-    valid_chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " "]
-
-    while valid_input is False:
-        puzzle = input("Input the sudoku: ")
-
-        # Catches invalid inputs
-        if len(puzzle) == 81:
-            if all(char in valid_chars for char in puzzle):
-                valid_input = True
-            else:
-                print("Invalid input type.")
-        else:
-            print("Invalid input length.")
-
-    # Stores the puzzle in a nested list with 0s as the blank tiles
-    puzzle = [puzzle[i:i+9] for i in range(0, len(puzzle), 9)]
-    puzzle = [[int(i) if i != " " else 0 for i in j] for j in puzzle]
-
-    # Prints initial sudoku and solved sudoku to user
-    print("\nInput sudoku:")
-    print_grid(puzzle)
-    start_time = timeit.default_timer()
-    solve(puzzle)
-    end_time = timeit.default_timer()
-
-    if solve(puzzle):
-        print("\nSolved sudoku:")
-        print_grid(puzzle)
-        print(f"\nTook {round(end_time - start_time, 5)} seconds!")
-    else:
-        print("\nNo solution found.")
-
-
-if __name__ == "__main__":
-    main()
